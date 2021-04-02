2075589bbd333ab379892a8b080e07586f233a11
李建民 <51262331+lijianmin01@users.noreply.github.com>
2021-04-02 17:07:32 +0800

    Delete zhi-neng-tui-jian directory
    1155

--- zhi-neng-tui-jian/XL/任务二/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-for user in users:
-        print("用户{}推荐的商品：".format(user))
-        for way in ways:
-            print("   方式{}：".format(way), end=" ")
-            ub = ItemCF(user_score_dict=user_score_dict,way=way)
-            print(ub.recommend(user))
